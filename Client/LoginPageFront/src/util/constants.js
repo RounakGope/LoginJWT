bc2e1813659c9c0401ae883d57d91a4d1e47cf30
Rounak Gope <165294367+RounakGope@users.noreply.github.com>
2025-10-18 13:11:57 +0530

    update constant 3

--- Client/LoginPageFront/src/util/constants.js
+++ Client/LoginPageFront/src/util/constants.js
@@ -3,8 +3,3 @@
 export const AppConstants = {
   BACKEND_URL: import.meta.env.VITE_API_URL || "http://localhost:8080/api/v1.0"
 };
-<<<<<<< HEAD
-=======
-=======
-
->>>>>>> 69598e00adedb3c42fa8d64672e22cb697baa127
